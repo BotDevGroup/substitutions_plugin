30eaffe49d127d8e4f192e019abf2332e0032086
Ricardo Arturo Cabral <ricardo.arturo.cabral@gmail.com>
2017-04-07 23:28:16 -0400

    First iteration.

---
+++ README.rst
@@ -0,0 +1,40 @@
+===============================
+Substitutions
+===============================
+
+
+.. image:: https://img.shields.io/pypi/v/substitutions.svg
+        :target: https://pypi.python.org/pypi/substitutions
+
+.. image:: https://img.shields.io/travis/cameri/substitutions.svg
+        :target: https://travis-ci.org/cameri/substitutions
+
+.. image:: https://readthedocs.org/projects/substitutions/badge/?version=latest
+        :target: https://substitutions.readthedocs.io/en/latest/?badge=latest
+        :alt: Documentation Status
+
+.. image:: https://pyup.io/repos/github/cameri/substitutions/shield.svg
+     :target: https://pyup.io/repos/github/cameri/substitutions/
+     :alt: Updates
+
+
+A plugin for marvinbot to substitute text matching regular expressions
+
+
+* Free software: MIT license
+* Documentation: https://substitutions.readthedocs.io.
+
+
+Features
+--------
+
+* TODO
+
+Credits
+---------
+
+This package was created with Cookiecutter_ and the `audreyr/cookiecutter-pypackage`_ project template.
+
+.. _Cookiecutter: https://github.com/audreyr/cookiecutter
+.. _`audreyr/cookiecutter-pypackage`: https://github.com/audreyr/cookiecutter-pypackage
+
---
+++ substitutions/base.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+from marvinbot.utils import get_message
+from marvinbot.plugins import Plugin
+from marvinbot.handlers import CommonFilters, MessageHandler
+import logging
+import re
+
+log = logging.getLogger(__name__)
+
+
+class Substitutions(Plugin):
+    def __init__(self):
+        super(Substitutions, self).__init__('substitutions')
+
+    def get_default_config(self):
+        return {
+            'short_name': self.name,
+            'enabled': True,
+            'pattern': r'^s/(?P<pattern>.+?)(?<!\\)/(?P<replacement>.*)$'
+        }
+
+    def configure(self, config):
+        self.pattern = re.compile(config.get('pattern'))
+        pass
+
+    def setup_handlers(self, adapter):
+        self.add_handler(MessageHandler([CommonFilters.text], self.on_text), priority=90)
+
+    def setup_schedules(self, adapter):
+        pass
+
+    def on_text(self, update):
+        message = get_message(update)
+        text = message.text
+
+        # Quit early if there's no text
+        if len(text) == 0:
+            return
+
+        # Filter messages that are not replies
+        if not message.reply_to_message:
+            return
+
+        # Filter replies to messages without text
+        if len(message.reply_to_message.text) == 0:
+            return
+
+        # Check if we are getting s//
+        match = self.pattern.match(text)
+        if not match:
+            return
+
+        user_pattern = match.group('pattern')
+        user_replacement = match.group('replacement')
+
+        # Try compiling user pattern
+        try:
+            pattern = re.compile(user_pattern, flags=re.IGNORECASE)
+            if not pattern:
+                return
+
+            response_text = pattern.sub("<b>{}</b>".format(user_replacement), message.reply_to_message.text)
+
+            self.adapter.bot.sendMessage(chat_id=message.chat_id,
+                                         text="<b>Did you mean</b>❔\n{}".format(response_text),
+                                         parse_mode='HTML')
+        except Exception as ex:
+            log.info("Exception when compiling user pattern: {}".format(ex))
